1ae08dca1915ae90e5b9cae66681b862177ecfc1
PC-FAYSSAL <fayssal.zf@gmail.com>
2024-06-13 09:18:58 +0100

    version mobile test

--- src/main/java/dev/fenix/application/GlobalVariables.java
+++ src/main/java/dev/fenix/application/GlobalVariables.java
@@ -2,8 +2,8 @@ package dev.fenix.application;
 
 public class GlobalVariables {
 
-    /// http://105.155.251.128/api/azure
-   // http://192.168.3.3:75/api/azure
+    /// 105.155.251.128
+   // 192.168.3.3:75
     public static final String IP_ADDRESS = "192.168.3.3:75";
     public static final String END_POINT = "http://" + IP_ADDRESS + "/api/azure";
 }
